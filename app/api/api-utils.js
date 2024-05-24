473e236954a218780bdc4b113ee30da35ea4ff7b
us4rGH <150263960+us4rGH@users.noreply.github.com>
2024-05-24 23:33:50 +0300

    fix: no data error

--- app/api/api-utils.js
+++ app/api/api-utils.js
@@ -37,9 +37,6 @@ export const getNormalizedGameDataById = async (url, id) => {
 export const getNormalizedGamesDataByCategory = async (url, category) => {
   try {
     const data = await getData(`${url}?categories.name=${category}`)
-    if (!data.length) {
-      throw new Error('Нет игр в категории')
-    }
     return isResponseOk(data) ? normalizeData(data) : data
   } catch (error) {
     return error
